50f9318ecc86a22f8c60015fdfa107834e2bfd76
sreekar <srikarkrishnag@gmail.com>
2024-07-05 21:27:14 -0500

    modified commit

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,10 @@ function App() {
       <div className="fixed top-0 -z-10 h-full w-full"></div>
       <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"></div>
       <div className="container mx-auto px-8">
+        
+        <div className='fixed top-0 left-0 right-0 bg-transparent z-10 px-8'>
         <Navbar/>
+        </div>
         <Hero/>
         <About/>
         <Technologies/>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   return (
     <nav className='mb-20 flex items-center justify-between py-6'>
       <div className="flex flex-shrink-0 items-center">
-        <img className="ml-5 h-14" src={logo} alt="logo" />
+        <img className="border ml-5 h-14" src={logo} alt="logo" />
       </div>
       <div className="mx-2 w-auto flex items-center justify-center gap-4 text-2xl">
         <FaLinkedin/>
